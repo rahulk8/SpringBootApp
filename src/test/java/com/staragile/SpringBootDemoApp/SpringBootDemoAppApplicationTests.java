b2b7db668b736da39387f4faebf985ee46f5d1f4
rahul <rahulpatna89@gmail.com>
2023-06-26 00:17:13 +0530

    1st commit for spring boot app

---
+++ src/test/java/com/staragile/SpringBootDemoApp/SpringBootDemoAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.staragile.SpringBootDemoApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootDemoAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
